4c056016989ab3d47d47b953ca4074c2f4477529
diana <gaponcic.d@gmail.com>
2021-02-11 02:20:46 +0200

    try to fix server connection restart

--- lib/server_conn.ex
+++ lib/server_conn.ex
@@ -1,17 +1,35 @@
 defmodule ServerConn do
-
+    
     def getTweet() do
         receive do
-            tweet ->
-                Router.route(tweet)
-                getTweet()
+            tweet -> 
+                # IO.inspect("new_ tweet")
+                # GenServer.cast(:Router, {:route, tweet})
         end
-    end
-
-    def start() do
-        IO.puts("starting server conn")
-        EventsourceEx.new("localhost:4000/tweets/1", stream_to: self())
         getTweet()
     end
 
+
+    def start_link() do
+        IO.puts("starting server conn")
+        handle = spawn_link(__MODULE__, :getTweet, [])
+        {:ok, old_pid} = EventsourceEx.new("http://localhost:4000/tweets/2", stream_to: handle)
+
+        spawn_link(__MODULE__, :checkConnection, ["http://localhost:4000/tweets/2", handle, old_pid])
+        {:ok, self()}
+    end
+
+    def checkConnection(url, handle, pid) do
+        Process.monitor(pid)
+        receive do
+            err ->
+                IO.puts("restarting")
+                {ok, new_pid} = EventsourceEx.new(url, stream_to: handle)
+                checkConnection(url, handle, new_pid)
+
+        end
+    end
 end
+
+
+
