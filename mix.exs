045a1bb481e0005072505a6dbbbf19de89db0547
diana <gaponcic.d@gmail.com>
2021-02-17 12:28:22 +0200

    add poison dependency

--- mix.exs
+++ mix.exs
@@ -1,26 +1,27 @@
 defmodule App.MixProject do
-  use Mix.Project
+    use Mix.Project
 
-  def project do
-    [
-      app: :app,
-      version: "0.1.0",
-      elixir: "~> 1.11",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
-    ]
-  end
+    def project do
+    	[
+			app: :app,
+			version: "0.1.0",
+			elixir: "~> 1.11",
+			start_permanent: Mix.env() == :prod,
+			deps: deps()
+      	]
+    end
 
-  def application do
-    [
-      extra_applications: [:logger],
-      mod: {App.Application, []}
-    ]
-  end
+    def application do
+      	[
+        	extra_applications: [:logger],
+        	mod: {App.Application, []}
+      	]
+    end
 
-  defp deps do
-    [
-      {:eventsource_ex, "~> 0.0.2"}
-    ]
-  end
+    defp deps do
+      	[
+			{:eventsource_ex, "~> 0.0.2"},
+			{:poison, "~> 3.1"}
+      	]
+    end
 end
