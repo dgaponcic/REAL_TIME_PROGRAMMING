7b26df0100d025eb4fba5995f8164e14d14a4535
diana <gaponcic.d@gmail.com>
2021-02-17 12:29:04 +0200

    add top hashtags child to main supervisor

--- lib/app/application.ex
+++ lib/app/application.ex
@@ -1,34 +1,52 @@
 defmodule App.Application do
-  use Application
-  
-  @impl true
-  def start(_type, _args) do
-    url1 = "http://localhost:4000/tweets/1"
-    url2 = "http://localhost:4000/tweets/2"
+    use Application
+    
+    @impl true
+    def start(_type, _args) do
+        url1 = "http://localhost:4000/tweets/1"
+        url2 = "http://localhost:4000/tweets/2"
 
-    children = [
-      %{
-        id: WorkerSupervisor,
-        start: {WorkerSupervisor, :start, []},
-      },
+        children = [
+            %{
+              	id: Registry,
+              	start: {Registry, :start_link, [:duplicate, Registry.ViaTest]}
+            },
 
-      %{
-        id: Router,
-        start: {Router, :start, []}
-      }, 
+            %{
+				id: AutoScaler,
+				start: {AutoScaler, :start_link, []}
+            },
 
-      %{
-        id: ServerConn1,
-        start: {ServerConn, :start_link, [url1]},
-      },
+            %{
+				id: WorkerSupervisor,
+				start: {WorkerSupervisor, :start_link, []},
+            },
 
-      %{
-        id: ServerConn2,
-        start: {ServerConn, :start_link, [url2]},
-      },
-    ]
+            %{
+			    id: Router,
+				start: {Router, :start_link, []}
+            }, 
 
-    opts = [strategy: :one_for_one, max_restarts: 100, name: App.Supervisor]
-    Supervisor.start_link(children, opts)
-  end
+            %{
+                id: ServerConn1,
+                start: {ServerConn, :start_link, [url1]},
+            },
+
+            %{
+                id: ServerConn2,
+                start: {ServerConn, :start_link, [url2]},
+            },
+
+            %{
+                id: TopHashtags,
+                start: {TopHashtags, :start_link, []},
+            },
+        ]
+
+
+        opts = [strategy: :one_for_one, max_restarts: 100, name: App.Supervisor]
+
+        Supervisor.start_link(children, opts)
+
+    end
 end
